8a9ea6a6db2e7968ea023cf12f3710e4b22740a9
NyanCatda <nyancatda@outlook.com>
2021-12-05 23:58:19 +0800

    完成制作

---
+++ MotdBEAPI/MotdBEAPI.go
@@ -0,0 +1,59 @@
+package MotdBEAPI
+
+import (
+	"encoding/hex"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/iancoleman/orderedmap"
+)
+
+//MotdBE API
+//Host 服务器地址 nyan.xyz:19132
+func MotdBE(Host string) (*orderedmap.OrderedMap) {
+	Jsondata := orderedmap.New()
+	if Host == "" {
+		Jsondata.Set("status", "offline")
+		return Jsondata
+	}
+
+	// 创建连接
+	socket, err := net.Dial("udp", Host)
+	if err != nil {
+		Jsondata.Set("status", "offline")
+		return Jsondata
+	}
+	defer socket.Close()
+	// 发送数据
+	time1 := time.Now().UnixNano() / 1e6 //记录发送时间
+	senddata, _ := hex.DecodeString("0100000000240D12D300FFFF00FEFEFEFEFDFDFDFD12345678")
+	_, err = socket.Write(senddata)
+	if err != nil {
+		Jsondata.Set("status", "offline")
+		return Jsondata
+	}
+	// 接收数据
+	UDPdata := make([]byte, 4096)
+	_, err = socket.Read(UDPdata)
+	if err != nil {
+		Jsondata.Set("status", "offline")
+		return Jsondata
+	}
+	time2 := time.Now().UnixNano() / 1e6 //记录接收时间
+	//解析数据
+	if err == nil {
+		MotdData := strings.Split(string(UDPdata), ";")
+		Jsondata.Set("status", "online")
+		Jsondata.Set("host", Host)             //服务器Host
+		Jsondata.Set("motd", MotdData[1])      //Motd
+		Jsondata.Set("agreement", MotdData[2]) //协议版本
+		Jsondata.Set("version", MotdData[3])   //游戏版本
+		Jsondata.Set("online", MotdData[4])    //在线人数
+		Jsondata.Set("max", MotdData[5])       //最大在线人数
+		Jsondata.Set("gamemode", MotdData[6])  //游戏模式
+		Jsondata.Set("delay", time2-time1)     //连接延迟
+	}
+
+	return Jsondata
+}
--- main.go
+++ main.go
@@ -1,52 +1,31 @@
 package main
 
 import (
-	"encoding/hex"
+	"blackbe.xyz/MotdBE/MotdBEAPI"
 	"fmt"
-	"net"
-	"strings"
-	"time"
+	"net/http"
 
-	"github.com/iancoleman/orderedmap"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	Jsondata := orderedmap.New()
-	// 创建连接
-	socket, err := net.DialUDP("udp4", nil, &net.UDPAddr{
-		IP:   net.IPv4(47, 100, 252, 210),
-		Port: 19132,
-	})
-	if err != nil {
-		Jsondata.Set("status", "offline")
-	}
-	defer socket.Close()
-	// 发送数据
-	time1 := time.Now().UnixNano() / 1e6 //记录发送时间
-	senddata, _ := hex.DecodeString("0100000000240D12D300FFFF00FEFEFEFEFDFDFDFD12345678")
-	_, err = socket.Write(senddata)
-	if err != nil {
-		Jsondata.Set("status", "offline")
-	}
-	// 接收数据
-	UDPdata := make([]byte, 4096)
-	_, _, err = socket.ReadFromUDP(UDPdata)
-	if err != nil {
-		Jsondata.Set("status", "offline")
-	}
-	time2 := time.Now().UnixNano() / 1e6 //记录接收时间
-	//解析数据
-	MotdData := strings.Split(string(UDPdata), ";")
-	if err == nil {
-		Jsondata.Set("status", "offline")
-		Jsondata.Set("motd", MotdData[1])      //Motd
-		Jsondata.Set("agreement", MotdData[2]) //协议版本
-		Jsondata.Set("version", MotdData[3])   //游戏版本
-		Jsondata.Set("online", MotdData[4])    //在线人数
-		Jsondata.Set("max", MotdData[5])       //最大在线人数
-		Jsondata.Set("gamemode", MotdData[6])  //游戏模式
-		Jsondata.Set("delay", time2-time1)     //连接延迟
-	}
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
 
-	fmt.Println(Jsondata)
+	r.LoadHTMLGlob("assets/*")
+	r.Static("/assets", "./assets")
+	r.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", gin.H{})
+    })
+
+	r.GET("/api", func(c *gin.Context) {
+		Host := c.Query("host")
+
+		data := MotdBEAPI.MotdBE(Host)
+		c.JSON(http.StatusOK, data)
+	})
+
+	if err := r.Run(":8080"); err != nil {
+		fmt.Println(err)
+	}
 }
