21b1f164ea694153373b730bb706f958a7a3976a
NyanCatda <nyancatda@outlook.com>
2022-01-03 13:32:51 +0800

    修改状态图样式

--- StatusImg/StatusImg.go
+++ StatusImg/StatusImg.go
@@ -1,7 +1,7 @@
 /*
  * @Author: NyanCatda
  * @Date: 2021-12-26 21:23:59
- * @LastEditTime: 2022-01-03 12:08:14
+ * @LastEditTime: 2022-01-03 13:26:27
  * @LastEditors: NyanCatda
  * @Description: 服务器状态图片生成
  * @FilePath: \MotdBE\StatusImg\StatusImg.go
@@ -61,7 +61,7 @@ func ServerStatusImg(Host string) *bytes.Buffer {
 	img := backgroundImg.(*image.NRGBA)
 
 	//读取字体数据
-	fontBytes, err := ioutil.ReadFile("StatusImg/SourceHanSansCN-VF.ttf")
+	fontBytes, err := ioutil.ReadFile("StatusImg/unifont-12.1.04.ttf")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -70,33 +70,46 @@ func ServerStatusImg(Host string) *bytes.Buffer {
 		fmt.Println(err)
 	}
 
+	//设置标题字体
 	f := freetype.NewContext()
 	//设置分辨率
 	f.SetDPI(72)
 	//设置字体
 	f.SetFont(font)
 	//设置尺寸
-	f.SetFontSize(40)
+	f.SetFontSize(30)
+	f.SetClip(img.Bounds())
+	//设置输出的图片
+	f.SetDst(img)
+	//设置字体颜色(白色)
+	f.SetSrc(image.NewUniform(color.RGBA{0, 0, 0, 255}))
+	pt := freetype.Pt(20, 30+int(f.PointToFixed(26))>>8)
+	f.DrawString("MOTD: "+RemoveColorCode(ServerData.Motd), pt)
+
+	//设置内容字体
+	f = freetype.NewContext()
+	//设置分辨率
+	f.SetDPI(72)
+	//设置字体
+	f.SetFont(font)
+	//设置尺寸
+	f.SetFontSize(30)
 	f.SetClip(img.Bounds())
 	//设置输出的图片
 	f.SetDst(img)
 	//设置字体颜色(白色)
 	f.SetSrc(image.NewUniform(color.RGBA{255, 255, 255, 255}))
-
-	//设置字体的位置
-	pt := freetype.Pt(10, 40+int(f.PointToFixed(26))>>8)
-	f.DrawString("MOTD: "+RemoveColorCode(ServerData.Motd), pt)
-	pt = freetype.Pt(10, 90+int(f.PointToFixed(26))>>8)
+	pt = freetype.Pt(20, 75+int(f.PointToFixed(26))>>8)
 	f.DrawString("协议版本: "+strconv.Itoa(ServerData.Agreement), pt)
-	pt = freetype.Pt(10, 140+int(f.PointToFixed(26))>>8)
+	pt = freetype.Pt(20, 125+int(f.PointToFixed(26))>>8)
 	f.DrawString("游戏版本: "+ServerData.Version, pt)
-	pt = freetype.Pt(10, 190+int(f.PointToFixed(26))>>8)
+	pt = freetype.Pt(20, 175+int(f.PointToFixed(26))>>8)
 	f.DrawString("在线人数: "+strconv.Itoa(ServerData.Online)+"/"+strconv.Itoa(ServerData.Max), pt)
-	pt = freetype.Pt(10, 240+int(f.PointToFixed(26))>>8)
+	pt = freetype.Pt(20, 225+int(f.PointToFixed(26))>>8)
 	f.DrawString("存档名字: "+RemoveColorCode(ServerData.LevelName), pt)
-	pt = freetype.Pt(10, 290+int(f.PointToFixed(26))>>8)
+	pt = freetype.Pt(20, 275+int(f.PointToFixed(26))>>8)
 	f.DrawString("游戏模式: "+GamemodeChinese(ServerData.GameMode), pt)
-	pt = freetype.Pt(10, 340+int(f.PointToFixed(26))>>8)
+	pt = freetype.Pt(20, 325+int(f.PointToFixed(26))>>8)
 	f.DrawString("连接延迟: "+strconv.FormatInt(ServerData.Delay, 10), pt)
 
 	//将图片写入Buffer
